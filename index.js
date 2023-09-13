ba5ed3f0eda3fdb7a00c805efa20091b93d7ba0a
YOUR NAME <YOUR@EMAIL.com>
2023-09-13 22:25:17 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(hello){
+    return hello.toUpperCase();
+}
+function whisper(hello){
+    return hello.toLowerCase();
+}
+function logShout(hello) {
+    console.log(hello.toUpperCase());
+    }
+function logWhisper(hello) {
+        console.log(hello.toLowerCase());
+      }
+      function sayHiToGrandma(string) 
+      {
+        if (string === string.toUpperCase()) {
+          return 'YES INDEED!';
+        } else if (string === 'I love you') {
+          return 'I love you too';
+        } else {
+          return "I can't hear you!";
+        }
+      }
